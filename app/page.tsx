ad6c98a22a3da2f2cdffe5d27d515af09090e36c
Zay Yar Tun <zyt.tharthar@gmail.com>
2024-08-27 00:09:14 +0800

    nextjs setup with shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
-import Header from "@/components/custom/header";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-    </main>
+    <div className="flex flex-col w-full">
+      <div className="flex flex-col bg-slate-500">
+        <div className="flex items-center justify-between p-4 px-28">
+          <Link href="/" className="font-semibold text-xl">
+            Zay Yar Tun
+          </Link>
+          <div>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className="">Home</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>Education</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>Projects</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>Certificates</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>About</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
