6eee6b30e0a8483fa3c228f5f388524ee24aaccb
Zay Yar Tun <zyt.tharthar@gmail.com>
2024-11-16 18:46:24 +0800

    resume button change

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 
@@ -5,39 +6,37 @@ export default function Home() {
   return (
     <div className="flex flex-col w-full">
       <div className="flex flex-col">
-        <div className="flex items-center justify-between px-16 mt-2">
-          <Link href="/" className="font-semibold text-xl p-2 px-4 bg-slate-800 text-white dark:text-black dark:bg-slate-100 rounded-lg">
+        <div className="flex items-center justify-between px-16 mt-6">
+          <Link href="/" className="font-semibold text-xl py-2 px-4 bg-slate-800 text-white dark:text-black dark:bg-slate-100 rounded-lg">
             Z
           </Link>
-          <NavigationMenu>
-            <NavigationMenuList className="flex w-96 justify-between items-center text-sm">
-              <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink>About</NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink>Experience</NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink>Work</NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink>Contact</NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink>Resume</NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+          <div className="flex items-center">
+            <NavigationMenu>
+              <NavigationMenuList className="flex w-80 justify-between items-center text-sm">
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink>About</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink>Experience</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink>Work</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink>Contact</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+            <Button className="ms-12">Resume</Button>
+          </div>
         </div>
       </div>
     </div>
